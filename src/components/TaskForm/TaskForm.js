7b61e20980d5039a4856f459a5a45b3c75e7c441
Benjamin Cerninak <benjamin.cerninak@gmail.com>
2022-11-29 12:42:15 +0100

    added part of UI

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,17 +1,20 @@
 import React from "react";
 import classes from "./TaskForm.module.css";
 import Button from "../UI/Button";
+import Card from "../UI/Card";
 const TaskForm = (props) => {
   return (
-    <form>
-      <label>Task</label>
-      <input type="text" />
-      <label>Deadline</label>
-      <input type="date" />
-      <label>Time</label>
-      <input type="time" step="300" />
-      <Button>Add task</Button>
-    </form>
+    <Card>
+      <form>
+        <label>Task</label>
+        <input type="text" />
+        <label>Deadline</label>
+        <input type="date" />
+        <label>Time</label>
+        <input type="time" step="300" />
+        <Button>Add task</Button>
+      </form>
+    </Card>
   );
 };
 export default TaskForm;
